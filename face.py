679a63454b363855f90039fd47253fe59b075703
adamblue99 <adambbifulco@gmail.com>
2023-01-25 09:37:13 -0500

    Timing Changes

--- face.py
+++ face.py
@@ -1,12 +1,14 @@
 # Selfie taking assistance for blind people
 import cv2
 import numpy as np
-import sched, time 
+import sched, time
 import threading
 from datetime import datetime
 import speech_recognition as sr
 import pyttsx3 as p3
 import pyaudio
+import pandas
+import time
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
@@ -18,35 +20,35 @@ def txt_to_speech(phrase):
 
 
 def command_menu():
-    computer = p3.init()
-    r = sr.Recognizer()
-    while True:
-        with sr.Microphone() as source:
-            r.adjust_for_ambient_noise(source, duration=2)
+	computer = p3.init()
+	r = sr.Recognizer()
+	command_start = time.time()
+	while True:
+		with sr.Microphone() as source:
+			r.adjust_for_ambient_noise(source, duration=2)
+			computer.say("Specify a position your face should appear in the selfie, or ask for a list of positions by simply stating list. Speak now!")
+			computer.runAndWait()
+			audio = r.listen(source)
+		try:
+			quad = r.recognize_google(audio);
+			quad = str.lower(quad)
+			if quad == "list":
+				computer.say("The list options are: top left, top right, bottom left, and bottom right")
+				computer.runAndWait()
+			elif quad in {"top left", "top right", "bottom left", "bottom right"}:
+				computer.say("Selected " + quad + ".")
+				computer.runAndWait()
+				break
+			else:
+				computer.say("Could not understand audio.")
+				computer.runAndWait()
 
-            computer.say("Specify a position your face should appear in the selfie, or ask for a list of "
-                         + "positions by simply stating list. Speak now!")
-            computer.runAndWait()
-            audio = r.listen(source)
-        try:
-            print("You said " + r.recognize_google(audio))
-            quad = r.recognize_google(audio);
-            quad = str.lower(quad)
-            if quad == "list":
-                computer.say("The list options are: top left, top right, bottom left, and bottom right")
-                computer.runAndWait()
-            elif quad in {"top left", "top right", "bottom left", "bottom right"}:
-                computer.say("Selected " + quad + ".")
-                computer.runAndWait()
-                break
-            else:
-                computer.say("Could not understand audio.")
-                computer.runAndWait()
-
-        except sr.UnknownValueError:
-            computer.say("Could not understand audio.")
-            computer.runAndWait()
-    return quad
+		except sr.UnknownValueError:
+			computer.say("Could not understand audio.")
+		computer.runAndWait()
+	command_end = time.time()
+	print(command_end - command_start)
+	return quad
 
 
 
@@ -54,7 +56,7 @@ def command_menu():
 
 
 def find_face(frame, bound_rect, no_face_flag):
-	grey_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY) 
+	grey_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 	all_face_coords = face_cascade.detectMultiScale(grey_frame, 1.2, 5) # detect the face, return coordinates, may have more than one face
 	move_right = False
 	move_up = False
@@ -65,12 +67,12 @@ def find_face(frame, bound_rect, no_face_flag):
 		face_coords = all_face_coords [0] # use only one face
 		(x1,y1,x2,y2) = bound_rect
 		(x, y, w, h) = face_coords
-		
+
 		# double check that this logic is correct, it's not
 		if ((x1 < x < x2 and y1 < y < y2) or # upper left corner of face
-		(x1 < x + w < x2 and y1 < y < y2) or # upper right corner of face
-		(x1 < x  < x2 and y1 < y + h < y2) or # lower right corner of face
-		(x1 < x + w < x2 and y1 < y + h < y2)): # lower left corner of face
+				(x1 < x + w < x2 and y1 < y < y2) or # upper right corner of face
+				(x1 < x  < x2 and y1 < y + h < y2) or # lower right corner of face
+				(x1 < x + w < x2 and y1 < y + h < y2)): # lower left corner of face
 			# then the face is in the specified rectangle and you can take a picture
 			txt_to_speech ("Hold position, taking picture in 5")
 			time.sleep(0.05)
@@ -89,17 +91,17 @@ def find_face(frame, bound_rect, no_face_flag):
 		#if x1 == 0 and x + w  > x2: # if the face is too much to the right
 		if x1 == 0 and x > x2: # if the face is too much to the right
 			# give feedback, "move head to the right"(will be the opposite on the screen) or "move computer to the left"
-			move_right = True 
+			move_right = True
 		#elif x1 != 0 and x+ w < x1: # if the face is too much to the left
 		elif x1 != 0 and x + w< x1: # if the face is too much to the left
 			# give feedback, "move head to the left" or "move computer to the left"
-			move_left = True 
+			move_left = True
 
 		#determine up/down orientation
 		#if bound_rect[1] == 0 and y2 < y + h :   # if the face is too low
 		if bound_rect[1] == 0 and y2 < y:   # if the face is too low
 			# give feedback, "move head up" or "pull screen down a little"  
-			move_up = True 
+			move_up = True
 		#elif y1 != 0 and y1 > y: # if the face is too high
 		elif y1 != 0 and y1 > (y +  h ): # if the face is too high
 			# give feedback, "move head down" or "push screen up a little"
@@ -115,7 +117,7 @@ def find_face(frame, bound_rect, no_face_flag):
 			case 0: # has not given instructions yet
 				# move left 
 				txt_to_speech("Move left")
-				no_face_flag = 1 
+				no_face_flag = 1
 				return (False, no_face_flag)
 			case 1: # told them to go left already
 				txt_to_speech("Go back to original position")
@@ -137,18 +139,18 @@ def find_face(frame, bound_rect, no_face_flag):
 
 	# determine which instruction to give, this may need to be changed so that it won't say down if you already are within that boundary
 	#if move_up and move_right: txt_to_speech("Move head up and right") 
-	if move_right: 
-		txt_to_speech("Move head right") 
+	if move_right:
+		txt_to_speech("Move head right")
 		no_face_flag = 0
-	elif move_left: 
-		txt_to_speech("Move head left") 
+	elif move_left:
+		txt_to_speech("Move head left")
 		no_face_flag = 1
-	elif move_up: 
+	elif move_up:
 		txt_to_speech("Move head up")
 		no_face_flag = 3
 	# set no_face_flag to 0
 	#	elif (not move_up) and move_right: txt_to_speech("Move head down and right") 
-	elif move_down: 
+	elif move_down:
 		txt_to_speech("Move head down")
 		no_face_flag = 2
 
@@ -161,7 +163,7 @@ def find_face(frame, bound_rect, no_face_flag):
 
 # end of fuctions
 
-repeat = True 
+repeat = True
 # repeat taking the picture from here
 while repeat:
 
@@ -191,22 +193,22 @@ while repeat:
 			bound_rect = (0, height_mid*1.5, width_mid/2, window_length)
 		case "bottom right":
 			bound_rect = (width_mid*1.5, height_mid*1.5, window_width, window_length)
-			# ask the user to say the command again
-			# think about adding another option if voice doesn't work, like maybe pressing the space bar a certain number of times?
-			# or press the space bar when the option you want is said
+		# ask the user to say the command again
+		# think about adding another option if voice doesn't work, like maybe pressing the space bar a certain number of times?
+		# or press the space bar when the option you want is said
 
 
 
 	while True:
 		timeout = time.time() + 3 #three seconds	 
 		while True:
-		# take and analyze a photo  
-			_, frame = cap.read() 
+			# take and analyze a photo
+			_, frame = cap.read()
 
 			# bound_frame outline
 			# get rid of later
 			cv2.rectangle(frame, (int(bound_rect [0]),int (bound_rect [1])), (int (bound_rect [2]),int (bound_rect [3])), (255,0,0),2)
-			cv2.imshow('capture',frame)	
+			cv2.imshow('capture',frame)
 			if cv2.waitKey(1) & 0xFF == ord('q') or time.time()>timeout:
 				break
 
@@ -219,8 +221,8 @@ while repeat:
 			_, frame = cap.read()
 			now = datetime.now()
 			dt_str = now.strftime("%d_%m_%Y_%H_%M_%S")
-			cv2.imwrite('selfie_' + dt_str + '.jpg', frame) 
-			
+			cv2.imwrite('selfie_' + dt_str + '.jpg', frame)
+
 			txt_to_speech ("Picture saved, would you like to take another picture?")
 			while True:
 				with sr.Microphone() as source:
@@ -231,7 +233,7 @@ while repeat:
 					y_n_reply = str.lower(y_n_reply)
 					if y_n_reply == "yes":
 						txt_to_speech("Taking another photo")
-						break 
+						break
 					elif y_n_reply == "no":
 						repeat = False
 						break
