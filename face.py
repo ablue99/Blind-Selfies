334b0193518b2cb8d7f89b6e63e600bda26321ae
MWO2 <112632848+MWO2@users.noreply.github.com>
2023-01-25 09:05:48 -0500

    Update face.py

--- face.py
+++ face.py
@@ -3,10 +3,10 @@ import cv2
 import numpy as np
 import sched, time 
 import threading
-
-
+from datetime import datetime
 import speech_recognition as sr
 import pyttsx3 as p3
+import pyaudio
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
@@ -17,43 +17,72 @@ def txt_to_speech(phrase):
 	computer.runAndWait()
 
 
-def find_face(frame, bound_rect):
+def command_menu():
+    computer = p3.init()
+    r = sr.Recognizer()
+    while True:
+        with sr.Microphone() as source:
+            r.adjust_for_ambient_noise(source, duration=2)
+
+            computer.say("Specify a position your face should appear in the selfie, or ask for a list of "
+                         + "positions by simply stating list. Speak now!")
+            computer.runAndWait()
+            audio = r.listen(source)
+        try:
+            print("You said " + r.recognize_google(audio))
+            quad = r.recognize_google(audio);
+            quad = str.lower(quad)
+            if quad == "list":
+                computer.say("The list options are: top left, top right, bottom left, and bottom right")
+                computer.runAndWait()
+            elif quad in {"top left", "top right", "bottom left", "bottom right"}:
+                computer.say("Selected " + quad + ".")
+                computer.runAndWait()
+                break
+            else:
+                computer.say("Could not understand audio.")
+                computer.runAndWait()
+
+        except sr.UnknownValueError:
+            computer.say("Could not understand audio.")
+            computer.runAndWait()
+    return quad
+
+
+
+
+
+
+def find_face(frame, bound_rect, no_face_flag):
 	grey_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY) 
 	all_face_coords = face_cascade.detectMultiScale(grey_frame, 1.2, 5) # detect the face, return coordinates, may have more than one face
-
-
-	while str(all_face_coords) == "()":
-		t1 = threading.Thread(target=txt_to_speech("No face found."))
-		all_face_coords = face_cascade.detectMultiScale(grey_frame, 1.2, 5)
-		time.sleep(5)
-
-
 	move_right = False
 	move_up = False
+	move_left = False
+	move_down = False
 
 	if all_face_coords != ():
 		face_coords = all_face_coords [0] # use only one face
 		(x1,y1,x2,y2) = bound_rect
 		(x, y, w, h) = face_coords
 		
-		#cv2.rectangle(frame, (x,y), (x+face_coords[2],y+ h),(0,255,255),2)
-		# put this series of if statements in a loop, while a photo hasn't been taken yet?
-		
-		#if (x1 < x < x2 and (y1 < y < y2) and 
-		#(x1 < x +face_coords[2]<x2) and (y1 < y + h < y2)):
-
 		# double check that this logic is correct, it's not
 		if ((x1 < x < x2 and y1 < y < y2) or # upper left corner of face
 		(x1 < x + w < x2 and y1 < y < y2) or # upper right corner of face
 		(x1 < x  < x2 and y1 < y + h < y2) or # lower right corner of face
 		(x1 < x + w < x2 and y1 < y + h < y2)): # lower left corner of face
-		    # then the face is in the specified rectangle and you can take a picture
-		    print ("success")
-		    return True # successful
-		
-		# bound_rect[0] = x1, br[1]= y1, br[2] = x2, br[3] = y2
-		# fc[0]=x, fc[1] = y, fc[2] = w, fc[3] = h
+			# then the face is in the specified rectangle and you can take a picture
+			txt_to_speech ("Hold position, taking picture in 5")
+			time.sleep(0.05)
+			txt_to_speech("4")
+			time.sleep(0.05)
+			txt_to_speech("3")
+			time.sleep(0.05)
+			txt_to_speech("2")
+			time.sleep(0.05)
+			txt_to_speech("1")
 
+			return (True, no_face_flag) # successful
 
 		# determine right/ left orientation
 		# will need to have something to skip these blocks of code if the top was completed successfully
@@ -64,7 +93,7 @@ def find_face(frame, bound_rect):
 		#elif x1 != 0 and x+ w < x1: # if the face is too much to the left
 		elif x1 != 0 and x + w< x1: # if the face is too much to the left
 			# give feedback, "move head to the left" or "move computer to the left"
-			move_right = False
+			move_left = True 
 
 		#determine up/down orientation
 		#if bound_rect[1] == 0 and y2 < y + h :   # if the face is too low
@@ -74,107 +103,150 @@ def find_face(frame, bound_rect):
 		#elif y1 != 0 and y1 > y: # if the face is too high
 		elif y1 != 0 and y1 > (y +  h ): # if the face is too high
 			# give feedback, "move head down" or "push screen up a little"
-			move_up = False 
+			move_down = True
+	else:
+		# no face detected
+		txt_to_speech("No face found")
+		#txt_to_speech("Move left")
+		#_, frame = cap.read() 
+		#grey_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY) 
+		#all_face_coords = face_cascade.detectMultiScale(grey_frame, 1.2, 5) # detect the face, return coordinates, may have more than one face
+		match no_face_flag:
+			case 0: # has not given instructions yet
+				# move left 
+				txt_to_speech("Move left")
+				no_face_flag = 1 
+				return (False, no_face_flag)
+			case 1: # told them to go left already
+				txt_to_speech("Go back to original position")
+				txt_to_speech("Move right")
+				no_face_flag = 2
+				return (False, no_face_flag)
+			case 2: # told them to go left and right 
+				txt_to_speech("Go back to original position")
+				txt_to_speech("Move up")
+				no_face_flag = 3
+				return (False, no_face_flag)
+			case 3: # told them to move left and right and up
+				txt_to_speech("Go back to original position")
+				txt_to_speech("Move down")
+				# add another warning
+				no_face_flag = 0
+				return (False, no_face_flag)
+
 
 	# determine which instruction to give, this may need to be changed so that it won't say down if you already are within that boundary
-	if move_up and move_right: print("Move head up and right") 
-	elif (not move_up) and move_right: print("Move head down and right") 
-	elif move_up and (not move_right): print("Move head up and left") 
-	elif (not move_up) and (not move_right): print("Move head down and left") #is this effective or slow? 
+	#if move_up and move_right: txt_to_speech("Move head up and right") 
+	if move_right: 
+		txt_to_speech("Move head right") 
+		no_face_flag = 0
+	elif move_left: 
+		txt_to_speech("Move head left") 
+		no_face_flag = 1
+	elif move_up: 
+		txt_to_speech("Move head up")
+		no_face_flag = 3
+	# set no_face_flag to 0
+	#	elif (not move_up) and move_right: txt_to_speech("Move head down and right") 
+	elif move_down: 
+		txt_to_speech("Move head down")
+		no_face_flag = 2
 
-	return False #did not take the photo
+	#	elif move_up and (not move_right): txt_to_speech("Move head up and left") 
+	#	elif (not move_up) and (not move_right): txt_to_speech("Move head down and left") #is this effective or slow? 
+
+	return (False, no_face_flag) #did not take the photo
 
 
 
+# end of fuctions
 
-cv2.namedWindow('capture', cv2.WINDOW_FULLSCREEN)
-window_coords = cv2.getWindowImageRect('capture')
-(_,_,window_width, window_length) = window_coords
-# 2= width, 3= length
-width_mid = window_width*0.5
-height_mid = window_length*0.5
+repeat = True 
+# repeat taking the picture from here
+while repeat:
+
+	cv2.namedWindow('capture', cv2.WINDOW_FULLSCREEN)
+	window_coords = cv2.getWindowImageRect('capture')
+	(_,_,window_width, window_length) = window_coords
+	# 2= width, 3= length
+	width_mid = window_width*0.5
+	height_mid = window_length*0.5
+
+	no_face_flag = 0
 
 
-# get the frame from the webcamera and change to grey
-cap = cv2.VideoCapture(0)
-# give starting prompt
+	# get the frame from the webcamera and change to grey
+	cap = cv2.VideoCapture(0)
+	# give starting prompt
+	quad_command = command_menu()
+
+	# quad_command = "bottom right"
+	# after it recieves a command
+	match quad_command: # boundary rectangle (x1, y1, x2, y2)
+		case "top left":
+			bound_rect = (0,0, width_mid/2, height_mid/2)
+		case "top right":
+			bound_rect = (width_mid*1.5, 0, window_width, height_mid/2)
+		case "bottom left":
+			bound_rect = (0, height_mid*1.5, width_mid/2, window_length)
+		case "bottom right":
+			bound_rect = (width_mid*1.5, height_mid*1.5, window_width, window_length)
+			# ask the user to say the command again
+			# think about adding another option if voice doesn't work, like maybe pressing the space bar a certain number of times?
+			# or press the space bar when the option you want is said
 
 
-#quad_command  = input("quadrant: ")
-quad_command = "bottom right"
 
-# after it recieves a command
-match quad_command: # boundary rectangle (x1, y1, x2, y2)
-	case "top left":
-		bound_rect = (0,0, width_mid/2, height_mid/2)
-	case "top right":
-		bound_rect = (width_mid*1.5, 0, window_width, height_mid/2)
-	case "bottom left":
-		bound_rect = (0, height_mid*1.5, width_mid/2, window_length)
-	case "bottom right":
-		bound_rect = (width_mid*1.5, height_mid*1.5, window_width, window_length)
-		# ask the user to say the command again
-		# think about adding another option if voice doesn't work, like maybe pressing the space bar a certain number of times?
-		# or press the space bar when the option you want is said
-
-	
-while True:
-	timeout = time.time() + 2 #two seconds	 
 	while True:
-	# take and analyze a photo  
-		_, frame = cap.read()
+		timeout = time.time() + 3 #three seconds	 
+		while True:
+		# take and analyze a photo  
+			_, frame = cap.read() 
 
-		# bound_frame outline
-		cv2.rectangle(frame, (int(bound_rect [0]),int (bound_rect [1])), (int (bound_rect [2]),int (bound_rect [3])), (255,0,0),2)
-		
-		'''
-		grey_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY) 
-		all_face_coords = face_cascade.detectMultiScale(grey_frame, 1.2, 5) # detect the face, return coordinates, may have more than one face
-		if all_face_coords != ():
-			face_coords = all_face_coords [0] # use only one face
-			cv2.rectangle(frame, (face_coords[0],face_coords[1]), (face_coords[0]+face_coords[2],face_coords[1]+face_coords[3]),(0,255,255),2)
-		'''
+			# bound_frame outline
+			# get rid of later
+			cv2.rectangle(frame, (int(bound_rect [0]),int (bound_rect [1])), (int (bound_rect [2]),int (bound_rect [3])), (255,0,0),2)
+			cv2.imshow('capture',frame)	
+			if cv2.waitKey(1) & 0xFF == ord('q') or time.time()>timeout:
+				break
 
-
-		cv2.imshow('capture',frame)	
-		if cv2.waitKey(1) & 0xFF == ord('q') or time.time()>timeout:
+		(successful, no_face_flag) = find_face(frame, bound_rect, no_face_flag)
+		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
 
-	#face_t = threading.Thread(target = find_face, args = (frame, bound_rect))
-	#	face_t.start()
-	successful = find_face(frame, bound_rect)
+		if successful: # needs to be changed to not just exit but to give a countdown then read and save the current frame
+			# then ask if the user wants to take another photo
+			_, frame = cap.read()
+			now = datetime.now()
+			dt_str = now.strftime("%d_%m_%Y_%H_%M_%S")
+			cv2.imwrite('selfie_' + dt_str + '.jpg', frame) 
+			
+			txt_to_speech ("Picture saved, would you like to take another picture?")
+			while True:
+				with sr.Microphone() as source:
+					r.adjust_for_ambient_noise(source, duration=2)
+					audio = r.listen(source)
+				try:
+					y_n_reply = r.recognize_google(audio);
+					y_n_reply = str.lower(quad)
+					if y_n_reply == "yes":
+						txt_to_speech("Taking another photo")
+						break 
+					elif y_n_reply == "no":
+						repeat = False
+						break
+					else:
+						txt_to_speech("Could not understand audio.")
 
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+				except sr.UnknownValueError:
+					computer.say("Could not understand audio.")
+					computer.runAndWait()
+				break
 
-
-	if successful: # needs to be changed to not just exit but to give a countdown then read and save the current frame
-		# then ask if the user wants to take another photo
-		print ("All done")
-		# would need to put a check here if they want to repeat it
-		break
-
-print ("Exiting")
+txt_to_speech ("Exiting")
 cap.release()
 cv2.destroyAllWindows()
 
 
 
 
-
-
-# add a feature that calculates the percent of the screen taken up by the face?
-# if it is too great or too small then tell the person to move closer or further away
-# add a countdown for when the photo is taken? maybe
-# could also add a pinging sound that can help the person orientate themselves to the position of the camera
-# sometimes it's the computer screen you need to move the position of, so it could tell the person to move or to move their screen in a certain way
-
-
-# have the space bar be the shutter button? 
-
-# be waiting for input, should there be a signal? Maybe a pinging sound? What is a good to go sound for blind people?
-# receive directional input from user, analyze where the face is, give corrective audio prompts for where to move (a little a lot etc)
-# have a prompt for if no face is sighted, like maybe turn on your lights or come closer? 
-
-
-# should it prompt when to stop? 
